4b1e11a6716d11b88bc28a31d79092399d9f3438
Carrizane <miguelcarrizo@upeu.edu.pe>
2021-06-16 15:59:40 +0000

    Title updated

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className="container mt-5">
-      <h1 className="text-center">CRUD Simple</h1>
+      <h1 className="text-center">TODO List</h1>
       <hr />
       <div className="row">
         <div className="col-8">
